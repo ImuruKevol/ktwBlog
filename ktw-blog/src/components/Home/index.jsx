06ba55d34b9269db3bbe474769a56988c98fd9f9
ImuruKevol <kwon3286@korea.ac.kr>
2020-05-23 16:00:58 +0900

    작동 확인!

---
+++ ktw-blog/src/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <main>
+      test!
+    </main>
+  )
+}
+
+export default Home
--- ktw-blog/src/index.js
+++ ktw-blog/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './utils/common.scss';
 import './index.css';
-import App from './App';
+import Home from './components/Home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
